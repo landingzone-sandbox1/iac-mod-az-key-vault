085adbfaa2d0be210fd21a66ccb5e49fc26cc733
Lorrin Ferdinand <lferdinand@microsoft.com>
2025-06-25 20:37:37 -0700

    read me added (#14)
    Co-authored-by: Aditya Varma <aditya.varma@lumen.com>

--- local.tf
+++ local.tf
@@ -1,5 +1,4 @@
 # TODO: insert locals here.
 locals {
-  role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  service_code_akv                   = "AZKV"
+  service_code_akv = "AZKV"
 }
--- versions.tf
+++ versions.tf
@@ -1,19 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.28.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy      = true
-      recover_soft_deleted_keys         = true
-      recover_soft_deleted_certificates = true
-      recover_soft_deleted_secrets      = true
     }
   }
 }
