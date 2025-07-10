4ad3586395929e17ebdb0764d6db40bd11663569
Lorrin Ferdinand <lferdinand@microsoft.com>
2025-07-09 22:23:38 -0700

    fix: revert local testing settings (#28)

--- versions.tf
+++ versions.tf
@@ -7,12 +7,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
